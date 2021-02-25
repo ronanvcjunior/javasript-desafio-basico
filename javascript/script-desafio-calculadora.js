458a52fa1ec2292691693dceb689d7921458520a
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-02-25 10:19:14 -0300

    desafio-calculadora

--- javascript/script-desafio-calculadora.js
+++ javascript/script-desafio-calculadora.js
@@ -1,11 +1,12 @@
-var valor1
-var valor2
-var operacao
-var positionValor1
-var positionValor2
-var positionOperacao
-
 function calcular(valor) {
+    if (document.getElementById('campo').value.indexOf('=') !== -1) {
+        if ((valor >= 0 && valor <= 9) || (valor === '.')) {
+            reset()
+        } else {
+            var posicao = document.getElementById('campo').value.indexOf('=') + 2
+            document.getElementById('campo').value = document.getElementById('campo').value.slice(posicao)
+        }
+    }
     
     if (valor >= 0 && valor <= 9) {
         document.getElementById('campo').value += valor
@@ -15,73 +16,27 @@ function calcular(valor) {
 
     } else if (valor === 'soma') {
         document.getElementById('campo').value += ' + '
-        operacao = 'soma'
     }else if (valor === 'subtracao') {
         document.getElementById('campo').value += ' - '
-        operacao = 'subtracao'
 
     }else if (valor === 'multiplicacao') {
         document.getElementById('campo').value += ' * '
-        operacao = 'multiplicacao'
 
     }else if (valor === 'divisao') {
         document.getElementById('campo').value += ' / '
-        operacao = 'divisao'
 
     } else if (valor === 'resultado') {
-        switch (operacao) {
-            case 'soma':
-                positionOperacao = document.getElementById('campo').value.indexOf('+')
-                break
-            
-            case 'subtracao':
-                positionOperacao = document.getElementById('campo').value.indexOf('-')
-                break
-
-            case 'multiplicacao':
-                positionOperacao = document.getElementById('campo').value.indexOf('*')
-                break
-
-            case 'divisao':
-                positionOperacao = document.getElementById('campo').value.indexOf('/')
-                break
-        }
-        
-        positionValor1 = parseFloat(positionOperacao) - 1
-        positionValor2 = parseFloat(positionOperacao) + 2
-        
-        valor1 = parseFloat(document.getElementById('campo').value.slice(0, positionValor1))
-        valor2 = parseFloat(document.getElementById('campo').value.slice(positionValor2, document.getElementById('campo').value.lenght))
-        
-        positionoperacao = document.getElementById('campo').value += ' = '
-
-        switch (operacao) {
-            case 'soma':
-                positionoperacao = document.getElementById('campo').value += (valor1 + valor2)
-                break
-            
-            case 'subtracao':
-                positionoperacao = document.getElementById('campo').value += (valor1 - valor2)
-                break
-
-            case 'multiplicacao':
-                positionoperacao = document.getElementById('campo').value += (valor1 * valor2)
-                break
-
-            case 'divisao':
-                positionoperacao = document.getElementById('campo').value += (valor1 / valor2)
-                break
-        }
+        var resultado = eval(document.getElementById('campo').value)
+        document.getElementById('campo').value += ' = ' + resultado
     }
 }
 
 function reset() {
-    valor1 = null
-    valor2 = null
-    operacao = null
-    positionValor1 = null
-    positionValor2 = null
-    positionOperacao = null
-
     document.getElementById('campo').value = ''
 }
+
+var str = '12 + 2 = 2'
+var index = str.indexOf('=')
+console.log(index);
+var slice = str.slice(index+2)
+console.log(slice);
